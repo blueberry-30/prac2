343e7cfbea27b4c04566a31b1466f221a51713c1
blueberry-30 <nihalgupta8104@gmail.com>
2025-09-29 21:44:31 +0530

    Add files via upload

---
+++ prac6/prac6/wait-for-postgres.sh
@@ -0,0 +1,13 @@
+set -e
+
+host="$1"
+shift
+cmd="$@"
+
+until PGPASSWORD=mypassword psql -h "$host" -U "myuser" -d "mydb" -c '\q'; do
+  >&2 echo "Postgres is unavailable - sleeping"
+  sleep 1
+done
+
+>&2 echo "Postgres is up - executing command"
+exec $cmd
